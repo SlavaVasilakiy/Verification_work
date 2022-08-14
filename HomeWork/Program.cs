fe83f53f6a0a2c38813ba847fa7833c82b58ec68
vasilakiy <slk55slk@gmail.com>
2022-08-14 19:42:44 +0300

    add Print method

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -13,3 +13,17 @@ void FindResult(string[] array1, string[] array2)
             }
         }
     }
+
+void PrintArray(string[] array)
+    {
+    for(int i = 0;i < array.Length;i++)
+        {
+        Console.Write($"{array[i]} ");
+        }
+    Console.WriteLine();
+    }
+
+FindResult(array1, array2);
+PrintArray(array2);
+
+Console.ReadKey();
